0b39efacf9b6c281948395c936dd6933f64c15b9
shubam2637 <shubham@thinkclock.com>
2020-08-14 04:34:08 +0530

    prefinal

--- product/forms.py
+++ product/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 
-from .models import Product
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=256, min_length=4, required=True, label=_('Name'),
+    phone_number = forms.CharField(max_length=12, min_length=10, required=True, label=_('Phone Number'),
                                  widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    username = forms.EmailField(max_length=50, label=_('E-mail'),
-                                widget=(forms.TextInput(attrs={'class': 'form-control'})))
+
     password1 = forms.CharField(label=_('Password'),
                                 widget=(forms.PasswordInput(attrs={'class': 'form-control'})),
                                 )
@@ -20,5 +17,5 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'username', 'password1', 'password2',)
+        fields = ('first_name','last_name','phone_number' ,'username', 'password1', 'password2',)
 
